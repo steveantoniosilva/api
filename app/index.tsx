f0c50cad1485e9f5fc66d5af9522799862872e03
Steve Silva <steveantoniosilva@gmail.com>
2024-03-07 18:53:04 -0500

    s

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const App = () => {
   const [message, setMessage] = useState('');
@@ -13,13 +13,43 @@ const App = () => {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}>
-      <TouchableOpacity onPress={fetchHello}>
-        <Text style={{ color: 'white' }}>Fetch Message</Text>
+    <View style={styles.view}>
+      <View style={styles.container}>
         <Text style={{ color: 'white' }}>{message}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={fetchHello}>
+          <Text style={styles.buttonText}>Fetch Message</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  button: {
+    paddingVertical: 11,
+    paddingHorizontal: 22,
+    borderRadius: 33,
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  buttonText: {
+      color: 'white',
+      fontSize: 18,
+  },
+});
 
 export default App;
