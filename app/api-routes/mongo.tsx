666812eaf5acbbce1655cc382e33adb52606605d
Steve Silva <steveantoniosilva@gmail.com>
2024-03-11 09:39:32 -0400

    s

--- app/api-routes/mongo.tsx
+++ app/api-routes/mongo.tsx
@@ -2,6 +2,13 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.EXPO_PUBLIC_MONGODB_URI;
 
-console.log('MONGODB_URI', MONGODB_URI);
+console.log('MONGODB_URI:', MONGODB_URI);
 
-mongoose.connect(MONGODB_URI);
+if (!MONGODB_URI) {
+  throw new Error('Missing MONGODB_URI environment variable');
+}
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(error => console.error('Error connecting to MongoDB:', error));
