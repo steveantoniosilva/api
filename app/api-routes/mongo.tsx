81beb85071d875caf4df4925bdf3b919eaf1918a
Steve Silva <steveantoniosilva@gmail.com>
2024-03-11 09:38:26 -0400

    s

--- app/api-routes/mongo.tsx
+++ app/api-routes/mongo.tsx
@@ -1,31 +1,7 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.EXPO_PUBLIC_MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env');
-}
+console.log('MONGODB_URI', MONGODB_URI);
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => {
-      console.log('Successfully Connected to DB');
-
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default dbConnect;
+mongoose.connect(MONGODB_URI);
---
+++ models/BlogModel.tsx
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const BlogModel = mongoose.model('Blog', blogSchema);
